98845207e07468ea4298b5d316c606ddff873801
Max Dychenko <101450663+madmaxche@users.noreply.github.com>
2023-01-16 10:37:37 +0200

    madmax

---
+++ lesson37/js/dist/script.dev.js
@@ -0,0 +1,18 @@
+"use strict";
+
+//Крапка з комою
+console.log('Вчимо');
+console.log('JS');
+console.log('Вчимо JS');
+['Вчимо', 'JS'].forEach(alert); //Вирази
+
+var num = 4;
+console.log(num);
+'Вчимо' + 'JS'; //Блок інструкцій
+
+function name() {
+  console.log('Вчимо');
+  console.log('JS');
+}
+
+name();
---
+++ lesson37/js/script.js
@@ -0,0 +1,18 @@
+//Крапка з комою
+console.log('Вчимо'); console.log('JS')
+
+console.log('Вчимо JS');
+['Вчимо','JS'].forEach(alert);
+
+
+//Вирази
+let num = 4;
+console.log(num);
+'Вчимо'+'JS';
+
+//Блок інструкцій
+function name() {
+	console.log('Вчимо');
+	console.log('JS');
+}
+name ();
