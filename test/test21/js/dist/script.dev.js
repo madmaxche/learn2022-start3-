c739eab492b706660bdcb988461ccfa1f855bf5c
Max Dychenko <101450663+madmaxche@users.noreply.github.com>
2023-01-17 09:44:37 +0200

    madmax

---
+++ test/test21/js/dist/script.dev.js
@@ -0,0 +1,10 @@
+"use strict";
+
+//Перемінна для збереження кольору очей користувача сайту
+var userEyesColor = 'blue';
+console.log(userEyesColor); //Присвоєння значення 'Вася' перемінної userName для перемінної user, та виведенння значеня в консоль
+
+var user = 'Степан',
+    userName = 'Вася';
+user = userName;
+console.log(user);
---
+++ test/test21/js/script.js
@@ -0,0 +1,7 @@
+//Перемінна для збереження кольору очей користувача сайту
+	let userEyesColor = 'blue';
+	console.log(userEyesColor);
+//Присвоєння значення 'Вася' перемінної userName для перемінної user, та виведенння значеня в консоль
+	let user = 'Степан', userName = 'Вася';
+	user = userName;
+	console.log(user);
