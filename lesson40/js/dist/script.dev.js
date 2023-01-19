f79cff57364806c1cdd0dd282bcf1114fff964c7
Max Dychenko <101450663+madmaxche@users.noreply.github.com>
2023-01-19 08:46:21 +0200

    madmax

---
+++ lesson40/js/dist/script.dev.js
@@ -0,0 +1,9 @@
+"use strict";
+
+//Терміни: унарний , бінарний , операнд
+//Операнд
+var userAge = 30 - 6;
+var userHeight = h - y; //Бінарні
+
+var userAges = 30 - 6;
+var userCash = -800;
---
+++ lesson40/js/script.js
@@ -0,0 +1,15 @@
+
+//Терміни: унарний , бінарний , операнд
+
+//Операнд
+
+let userAge = 30 - 6;
+
+let userHeight = h - y;
+
+
+//Бінарні
+
+let userAges = 30 - 6 ;
+
+let userCash = -800;
