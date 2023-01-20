f300f7a171e6ab8eae0b28a5aa5e31deb819fd59
Max Dychenko <101450663+madmaxche@users.noreply.github.com>
2023-01-20 09:15:33 +0200

    madmax

--- lesson41/js/script.js
+++ lesson41/js/script.js
@@ -27,3 +27,74 @@ if(2 + 1 === 3 && "1" == 1 || 10>5 && 10 ===1){
 	//вираз в дужках поверне значення true
 	console.log(messagess);
 }
+
+//Блоки ELSE та ELSE IF
+let messagesss = "Привіт, фрілансер!"
+let number = 5;
+
+if(number > 10){
+	console.log(messagesss);
+} else{
+	console.log("Умова не виконана");
+}
+
+
+let messagessss = "Привіт, фрілансер!"
+let numbers = 5;
+if(numbers > 50){
+	console.log('5 більше 50');
+} else if (numbers > 30){
+	console.log('5 більше 30');
+} else if (numbers > 10){
+	console.log('5 більше 10');
+} else if (numbers > 1){
+	console.log('5 більше 1');
+} else{
+	console.log('Умова не виконана');
+}
+
+
+//Умовний оператор '?'
+let messageS = "Привіт"
+/*
+let messageSEnd;
+if(5 > 1){
+	messageSEnd = ", Максим!";
+} else {
+	messageSEnd = ", Юлія!";
+}
+*/
+
+let messageSEnd = 5 > 10 ? ", Максим!" : ", Юлія!";
+
+messageS += messageSEnd;
+console.log(messageS);
+
+messageSEnd = 5 > 10 ? ", Максюша" :
+	5 > 30 ? ",Сашка" :
+		5 > 10 ? ",Юляшка" :
+			5 > 1 ? ",Петрик" : ",Ромашка"
+
+messageS += messageSEnd;
+console.log(messageS);
+
+if(1 === "1"){
+	console.log('Правда!');
+} else{
+	console.log('Неправда!');
+}
+
+if(5 == "5"){
+	console.log('Правда!');
+} else{
+	console.log('Неправда!');
+}
+
+let messageSS = (92 > '11' && 58 < 100) ? 'Правда!' : 'Неправда!';
+console.log(messageSS);
+
+if (0){
+	console.log('Неправда!');
+} else if (" "){
+	console.log('Правда!');
+}
