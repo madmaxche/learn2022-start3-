77514592c81b4f6a63822ae539b9904228a73aea
Max Dychenko <101450663+madmaxche@users.noreply.github.com>
2023-01-21 11:28:01 +0200

    madmax

--- lesson42/js/dist/script.dev.js
+++ lesson42/js/dist/script.dev.js
@@ -1,5 +1,3 @@
-"use strict";
-
 // Цикл WHILE
 
 /*
@@ -56,17 +54,103 @@ for (Начало; Умова; Крок){
 //Можна прибрати любу частину циклу
 
 /*
-for(let num = 0; num < 5; num++){
+for (num = 0; num < 5; num++) {
 	console.log(num);
 }
-console.log('Вивід поза циклом: ${num}');
+console.log(`Вивід поза циклом: ${num}`);
 */
-//Дериктива break
-var num = 0;
+//Директива break
 
-for (; num < 5; num++) {
-  console.log(num);
-  if (num == 2) break;
+/*
+let num = 0 ;
+for(; num<5;num++){
+	console.log(num);
+	if(num==2)break;
+}
+console.log(`Робота завершена, num = ${num}`);
+*/
+//Директива continue
+
+/*
+let num = 0;
+for(; num < 5; num++){
+	if(num ==2) continue;
+	console.log(num);
+}
+*/
+//Мітки
+
+/*
+firstFor: for (let num = 0; num < 2; num++){
+	for(let size = 0; size < 3; size++){
+		if(size == 2){
+			break firstFor;
+		}
+		console.log(size);
+	}
+}
+*/
+
+/*
+firstFor: for (let num = 0; num < 2; num++){
+	for(let size = 0; size < 3; size++){
+		if(size == 2){
+			continue firstFor;
+		}
+		console.log(size);
+	}
+}
+*/
+//ДОМАШКА
+
+/*
+1. Изучить теорию
+2. Решить задачи:
+*/
+//Задача №1
+//Вывести в консоль числа от 1 до 5
+
+/*
+let nums = 1;
+while (nums<6){
+	console.log(nums);
+	nums++;
+}
+*/
+//Задача №2
+
+/*
+let num = 8;
+while (num) {
+	//Последний результат 0, верно?
+	console.log(num);
+	num--;
+}*/
+//Задача №3. Переписать на while
+
+/*
+for (let num = 0; num < 3; num++) {
+	console.log(`Число: ${num}`);
 }
 
-console.log('Робота закінчена, num = ${num}');
+let num = 0;
+while(num < 3){
+	console.log(`Число: ${num}`);
+	num++;
+}
+*/
+//Задача №4. Прекратить работу цикла №1 когда size равна 1
+//Цикл №1
+
+/*
+firstFor:for (let num = 0; num < 2; num++) {
+	//Цикл №2
+	for (let size = 0; size < 3; size++) {
+		if(size == 2){
+			break firstFor
+		}
+		console.log(size);
+	}
+}
+*/
+"use strict";
