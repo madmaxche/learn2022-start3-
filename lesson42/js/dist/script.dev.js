c491bf45a21572646088dd3ee998d7fa6be3988f
Max Dychenko <101450663+madmaxche@users.noreply.github.com>
2023-01-21 10:26:55 +0200

    madmax

---
+++ lesson42/js/dist/script.dev.js
@@ -0,0 +1,56 @@
+"use strict";
+
+// Цикл WHILE
+
+/*
+Синтаксис
+while(Умови){
+	Тіло цикла
+	Тут буде виконуватись код
+}
+*/
+//Приклад
+
+/*
+let num = 0;
+while (num<5){
+	console.log(num);
+	num++;
+}
+*/
+
+/*
+let num = 5;
+while (num){
+	console.log(num);
+	num--;
+}
+*/
+//Упрощений запис
+
+/*
+let num = 5 ;
+while (num) console.log(num--);
+*/
+//Конструкція DO...WHILE
+
+/*
+let num = 0;
+do{
+	console.log(num);
+	num++;
+} while (num < 5);
+*/
+//Цикл FOR
+//Синтаксис
+
+/*
+for (Начало; Умова; Крок){
+	Тіло цикла
+	Тут буде виконуватись код
+}
+*/
+//Приклад
+for (var num = 0; num < 5; num++) {
+  console.log(num);
+}
