70a598ce432c1b602cb5cbd71d9d192afe3a5d79
Max Dychenko <101450663+madmaxche@users.noreply.github.com>
2023-01-22 15:01:03 +0200

    madmax

---
+++ test/test22/js/script.js
@@ -0,0 +1,32 @@
+
+
+
+function calcSumm(numOne,numTwo) {
+	calcSumm = numOne + numTwo;
+	console.log(calcSumm);
+}
+calcSumm(1,6);
+
+function calcMinus(numOne,numTwo) {
+	calcMinus = numOne - numTwo;
+	console.log(calcMinus);
+}
+calcMinus(19,2);
+
+function calcMnojenya(numOne, numTwo) {
+	calcMnojenya = numOne * numTwo;
+	console.log(calcMnojenya);
+}
+calcMnojenya(5,2);
+
+function calcDilenya(numOne, numTwo) {
+	calcDilenya = numOne / numTwo;
+	console.log(calcDilenya);
+}
+calcDilenya(20,4);
+
+function calcVidsotok(numOne, numTwo) {
+	calcVidsotok = numOne % numTwo;
+	console.log(calcVidsotok);
+}
+calcVidsotok(5,100);
