8552c7e0b6d52ac5ec087e9050ae3a4bdcdabd53
Max Dychenko <101450663+madmaxche@users.noreply.github.com>
2023-02-07 21:37:15 +0200

    madmax

--- lesson49/js/dist/script.dev.js
+++ lesson49/js/dist/script.dev.js
@@ -13,4 +13,43 @@ console.log(mainElementHeight); //-------------
 var windowWidth = window.innerWidth;
 var windowHeight = window.innerHeight;
 console.log(windowWidth);
-console.log(windowHeight);
+console.log(windowHeight); // Ширина та висота документу
+// включаючи прокрученну область
+
+var scrollWidth = Math.max(document.body.scrollWidth, document.documentElement.scrollWidth, document.body.offsetHeight, document.documentElement.offsetWidth, document.body.clientWidth, document.documentElement.clientWidth);
+var scrollHeight = Math.max(document.body.scrollHeight, document.documentElement.scrollHeight, document.body.offsetHeight, document.documentElement.offsetHeight, document.body.clientHeight, document.documentElement.clientHeight);
+console.log(scrollWidth);
+console.log(scrollHeight); // Отримати кількість прокручених пікселів
+// Тільки для читання
+
+var windowScrollTop = window.pageYOffset;
+var windowScrollLeft = window.pageXOffset;
+console.log(windowScrollTop);
+console.log(windowScrollLeft); // Управління прокруткою сторінки
+// Метод scrollBy(x,y) прокручує сторінку відносно
+// її теперішнього положення
+
+function setScrollBy() {
+  window.scrollBy(0, 50);
+  var windowScrollTop = window.pageYOffset;
+  console.log(windowScrollTop);
+} // Метод scrollTo(pageX,pageY) прокручує сторінку 
+// на абсолютні координати (pageX,pageY)
+// те саме що і window.scroll()
+
+
+function setScrollTo() {
+  window.scrollTo(0, 50);
+  var windowScrollTop = window.pageYOffset;
+  console.log(windowScrollTop);
+}
+
+function setScrollToOptions() {
+  window.scrollTo({
+    top: 500,
+    left: 0,
+    // smooth, instant
+    // або auto; по замовчуванню auto
+    behavior: "smooth"
+  });
+} // Опції не працюють в Safari
--- lesson49/js/script.js
+++ lesson49/js/script.js
@@ -21,3 +21,61 @@ const windowHeight = window.innerHeight;
 
 console.log(windowWidth);
 console.log(windowHeight);
+
+// Ширина та висота документу
+// включаючи прокрученну область
+
+let scrollWidth = Math.max(
+	document.body.scrollWidth, document.documentElement.scrollWidth,
+	document.body.offsetHeight, document.documentElement.offsetWidth,
+	document.body.clientWidth, document.documentElement.clientWidth
+);
+let scrollHeight = Math.max(
+	document.body.scrollHeight, document.documentElement.scrollHeight,
+	document.body.offsetHeight, document.documentElement.offsetHeight,
+	document.body.clientHeight, document.documentElement.clientHeight
+);
+console.log(scrollWidth);
+console.log(scrollHeight);
+
+// Отримати кількість прокручених пікселів
+// Тільки для читання
+
+const windowScrollTop = window.pageYOffset;
+const windowScrollLeft = window.pageXOffset;
+
+console.log(windowScrollTop);
+console.log(windowScrollLeft);
+
+// Управління прокруткою сторінки
+
+// Метод scrollBy(x,y) прокручує сторінку відносно
+// її теперішнього положення
+
+function setScrollBy() { 
+	window.scrollBy(0,50);
+	const windowScrollTop = window.pageYOffset;
+	console.log(windowScrollTop);
+}
+
+// Метод scrollTo(pageX,pageY) прокручує сторінку 
+// на абсолютні координати (pageX,pageY)
+// те саме що і window.scroll()
+
+function setScrollTo() { 
+	window.scrollTo(0,50);
+	const windowScrollTop = window.pageYOffset;
+	console.log(windowScrollTop);
+}
+
+
+function setScrollToOptions(){
+	window.scrollTo({
+		top:500,
+		left:0,
+		// smooth, instant
+		// або auto; по замовчуванню auto
+		behavior:"smooth"
+	});
+}
+// Опції не працюють в Safari
