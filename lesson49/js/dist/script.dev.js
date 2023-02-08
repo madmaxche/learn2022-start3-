3e89d858535645feb7f1352b31ed5cf6dd605e22
Max Dychenko <101450663+madmaxche@users.noreply.github.com>
2023-02-08 11:26:01 +0200

    madmax

--- lesson49/js/dist/script.dev.js
+++ lesson49/js/dist/script.dev.js
@@ -53,3 +53,148 @@ function setScrollToOptions() {
     behavior: "smooth"
   });
 } // Опції не працюють в Safari
+// Управління прокруткою сторінки
+
+
+function setScrollIntoView(top) {
+  var lessonSelected = document.querySelector('.lesson_selected');
+  lessonSelected.scrollIntoView(top);
+}
+
+function setScrollIntoViewOptions(top) {
+  var lessonSelected = document.querySelector('.lesson__selected');
+  lessonSelected.scrollIntoView({
+    // start, "center", "end" або "nearest". По замовчуванню "center"
+    block: "center",
+    // start, "center", "end" або "nearest". По замовчуванню "nearest"
+    inline: "nearest",
+    // "auto" або "smooth". По замовчуванню "auto"
+    behavior: "smooth"
+  });
+} // Заборона прокрутки 
+
+
+function setEnableDisableScroll() {
+  //document.body.style.overflow = "hidden";
+  document.body.classList.toggle('scroll-lock');
+} // Метрики елементів на сторінці
+// Отримуєм об'єкт
+
+
+var block = document.querySelector('.lesson__block'); // Позиція об'єкту
+// Властивості offsetParent, offsetLeft, та offsetTop
+// Отримуємо батьківській елемент
+// відносно якого позиційований наш об'єкт
+
+/*/
+const elementOffsetParent = block.offsetParent;
+
+console.log(elementOffsetParent);
+*/
+// Отримуємо позицію об'єкта відносно предка (offsetParent)
+
+/*
+const elementOffsetLeft = block.offsetLeft;
+const elementOffsetTop = block.offsetTop;
+
+console.log(elementOffsetLeft);
+console.log(elementOffsetTop);
+*/
+// Отримуємо розміри об'єкта
+
+/*
+const elementOffsetWidth = block.offsetWidth;
+const elementOffsetHeight = block.offsetHeight;
+
+console.log(elementOffsetWidth);
+console.log(elementOffsetHeight);
+*/
+// Метрики для не відобраючих елементів рівні нулю
+// Отримуєм об'єкт
+
+var blockTopLeft = document.querySelector('.lesson__block'); // Відступи внутрішньої частини елемента від зовнішньої
+// clientTop та clientLeft
+
+/*
+const elementClientTop = blockTopLeft.clientTop;
+const elementClientLeft = blockTopLeft.clientLeft;
+
+console.log(elementClientTop);
+console.log(elementClientLeft);
+*/
+// Отримуєм об'єкт
+
+var blockWidthHeight = document.querySelector('.lesson__block'); // Розміри об'єкта без рамок та полоси прокрутки
+// clientWidth та clientHeight
+
+/*
+const elementClientWidth = blockWidthHeight.clientWidth;
+const elementClientHeight = blockWidthHeight.clientHeight;
+
+console.log(elementClientWidth);
+console.log(elementClientHeight);
+*/
+// загальна ширина - рамка ліворуч - рамка праворуч - скролл
+// 500 - 20 - 20 - 17 = 443
+// Отримуєм об'єкт
+
+/*
+const blockScroll= document.querySelector('.lesson__block');
+
+// Розміри об'єкта включаючи в себе
+// прокрученну (яку не видно) частину
+// В іншому працюють як clientWidth/clientHeight,
+//scrollWidth/scrollHeight
+
+const elementScrollWidth = blockScroll.scrollWidth;
+const elementScrollHeight = blockScroll.scrollHeight;
+
+console.log(elementScrollWidth);
+console.log(elementScrollHeight);
+
+// Розміри прокрученої области
+// scrollLeft / scrollTop
+
+//blockScroll.scrollTop = 150;
+
+const elementScrollLeft = blockScroll.scrollLeft;
+const elementScrollTop = blockScroll.scrollTop;
+
+console.log(elementScrollLeft);
+console.log(elementScrollTop);
+*/
+
+/*
+// Отримуєм об'єкт
+const blockScrollBy= document.querySelector('.lesson__block');
+
+// Методи управління прокруткою
+// scrollBy, scrollTo та scrollIntoView
+// працюють і для об'єкта
+
+function setElementScrollBy() {
+	blockScrollBy.scrollBy({
+		top:20,
+		left:0,
+		behavior:"smooth"
+	})
+}
+*/
+// Координати відносно вікна браузера
+// getBoundingClientRect
+// Отримуємо об'єкт
+
+var item = document.querySelector('.lesson__item'); // Отримуємо координати відно вікна браузера
+
+var getItemCoords = item.getBoundingClientRect();
+console.log(getItemCoords); // Отримуємо конкретну координату відносно вікна браузера
+
+var getItemLeftCoord = item.getBoundingClientRect().left; // Отримуємо конкретну координату відносно документа
+
+var getItemTopDocumentCoord = getItemCoord + window.pageYOffset;
+console.log(getItemLeftCoord);
+console.log(getItemTopDocumentCoord); // Отримання об'єкта по координатам
+// document.elementFromPoint(x,y);
+
+var elem = document.elementFromPoint(100, 100);
+console.log(elem);
